1ccd292172420ce826212a95b3578b1742830751
coltenappleby <colten.appleby@gmail.com>
2021-03-25 17:11:24 -0400

    commit tree 364aed76de704b87f4f18d205ab6de0036678ad5

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,19 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   Mayor.create(name: 'Emanuel', city: cities.first
+
+Passenger.destroy_all
+Taxi.destroy_all
+Ride.destroy_all
+
+1..50.times do
+    Passenger.create()
+end
+1..50.times do 
+    Taxi.create()
+end
+1..100.times do 
+    Ride.create(passenger: Passenger.all[rand(1..50)], taxi: Taxi.all[rand(1..50)])
+end
+
